a8063421621bf7a6429068f6443a33354bdcf274
vinay-parekh <bvp0802@gmail.com>
2024-07-25 11:51:50 +0530

    initial setup

---
+++ src/main/java/com/example/end2endapp/EndToEndApplication.java
@@ -0,0 +1,15 @@
+package com.example.end2endapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndToEndApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EndToEndApplication.class, args);
+		System.out.println("this project is working absolutely fine!!");
+	}
+
+}
---
+++ src/test/java/com/example/end2endapp/EndToEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.end2endapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndToEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
