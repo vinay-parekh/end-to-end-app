a2aeaee679cdb263944ec795eec3e41e870649a9
vinay-parekh <bvp0802@gmail.com>
2024-07-26 19:50:52 +0530

    implemented User Controller

---
+++ src/main/java/com/example/end2endapp/client/ClientController.java
@@ -0,0 +1,21 @@
+package com.example.end2endapp.client;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/clients")
+@RequiredArgsConstructor
+public class ClientController {
+
+    private final ClientService clientService;
+
+    @GetMapping
+    public String getClient(Model model) {
+        model.addAttribute("client", clientService.getAllClients());
+        return "clients";
+    }
+}
