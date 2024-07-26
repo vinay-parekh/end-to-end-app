ced60999c0a2ae03701cb9b33c14af6276e62fb7
vinay-parekh <bvp0802@gmail.com>
2024-07-26 19:51:21 +0530

    Home Page Setup

---
+++ src/main/java/com/example/end2endapp/home/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.end2endapp.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String homePage() {
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
