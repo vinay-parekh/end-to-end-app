fee4a6f040e742e428e59d38af28d0cc25c14d38
vinay-parekh <bvp0802@gmail.com>
2024-07-26 19:45:54 +0530

    configured the JPA properties for Client

---
+++ src/main/java/com/example/end2endapp/client/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.example.end2endapp.client;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+    Optional<Client> findByEmail(String email);
+}
