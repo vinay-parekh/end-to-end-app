d181b3f18889cd7828415cff234fc747a39d5308
vinay-parekh <bvp0802@gmail.com>
2024-07-27 09:29:37 +0530

    implemented Verification Token Service

---
+++ src/main/java/com/example/end2endapp/registration/token/VerificationTokenService.java
@@ -0,0 +1,12 @@
+package com.example.end2endapp.registration.token;
+
+import com.example.end2endapp.client.Client;
+
+import java.util.Optional;
+
+public interface VerificationTokenService {
+
+    String validateToken(String token);
+    void saveVerificationTokenForClient(Client client, String token);
+    Optional<VerificationToken> findByToken(String token);
+}
---
+++ src/main/java/com/example/end2endapp/registration/token/VerificationTokenServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.end2endapp.registration.token;
+
+import com.example.end2endapp.client.Client;
+import com.example.end2endapp.client.ClientRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class VerificationTokenServiceImpl implements VerificationTokenService{
+
+    private final VerificationTokenRepository tokenRepository;
+    private final ClientRepository clientRepository;
+
+    @Override
+    public String validateToken(String token) {
+        Optional<VerificationToken> theToken = tokenRepository.findByToken(token);
+        if (theToken.isEmpty()) {
+            return "invalid";
+        }
+        Client client = theToken.get().getClient();
+        Calendar calendar = Calendar.getInstance();
+        if ((theToken.get().getExpirationTime().getTime()
+                - calendar.getTime().getTime() <= 0)) {
+            return "expired";
+        }
+        client.setEnabled(true);
+        clientRepository.save(client);
+        return "valid";
+    }
+
+    @Override
+    public void saveVerificationTokenForClient(Client client, String token) {
+        var verificationToken = new VerificationToken(token, client);
+        tokenRepository.save(verificationToken);
+    }
+
+    @Override
+    public Optional<VerificationToken> findByToken(String token) {
+        return tokenRepository.findByToken(token);
+    }
+}
