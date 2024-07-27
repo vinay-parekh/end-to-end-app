aabfd08cfa927f2f5808808780436bf3b6923dd5
vinay-parekh <bvp0802@gmail.com>
2024-07-27 11:13:59 +0530

    refactor: Token Expiration Time modified

--- src/main/java/com/example/end2endapp/registration/token/TokenExpirationTime.java
+++ src/main/java/com/example/end2endapp/registration/token/TokenExpirationTime.java
@@ -5,7 +5,7 @@ import java.util.Date;
 
 public class TokenExpirationTime {
 
-    private static final int EXPIRATION_TIME = 10;
+    private static final int EXPIRATION_TIME = 1;
 
     public static Date getExpirationTime() {
         Calendar calendar = Calendar.getInstance();
