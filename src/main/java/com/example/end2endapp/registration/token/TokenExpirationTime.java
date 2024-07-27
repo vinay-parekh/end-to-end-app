2c106e5232d31a5fbc30f4f8377787d5b21ea1ee
vinay-parekh <bvp0802@gmail.com>
2024-07-27 09:30:08 +0530

    threshold time for Token setup completed

---
+++ src/main/java/com/example/end2endapp/registration/token/TokenExpirationTime.java
@@ -0,0 +1,16 @@
+package com.example.end2endapp.registration.token;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class TokenExpirationTime {
+
+    private static final int EXPIRATION_TIME = 10;
+
+    public static Date getExpirationTime() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(new Date().getTime());
+        calendar.add(Calendar.MINUTE, EXPIRATION_TIME);
+        return new Date(calendar.getTime().getTime());
+    }
+}
