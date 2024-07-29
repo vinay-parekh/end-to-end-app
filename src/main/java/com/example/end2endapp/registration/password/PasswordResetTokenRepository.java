0fa0b1098e12b5fbe92900194c829b35bac8349f
vinay-parekh <bvp0802@gmail.com>
2024-07-29 16:51:49 +0530

    JPA features integrated with the entities

---
+++ src/main/java/com/example/end2endapp/registration/password/PasswordResetTokenRepository.java
@@ -0,0 +1,9 @@
+package com.example.end2endapp.registration.password;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+    Optional<PasswordResetToken> findByToken(String theToken);
+}
